fdc9f4a0f8a60fb76272ea2409cffa2685e0826f
Yu Xin <yuxin@YudeMac-mini.local>
2025-10-06 14:03:32 +0800

    first commit

---
+++ cmake-build-debug/src/CMakeFiles/logger_manager.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "/Volumes/WD_BLACK/Coding/Project/CPP/Logger/lib/liblogger_manager.dylib"
+  "/Volumes/WD_BLACK/Coding/Project/CPP/Logger/lib/liblogger_manager.pdb"
+  "CMakeFiles/logger_manager.dir/glogmessage.cpp.o"
+  "CMakeFiles/logger_manager.dir/glogmessage.cpp.o.d"
+  "CMakeFiles/logger_manager.dir/logggerfactory.cpp.o"
+  "CMakeFiles/logger_manager.dir/logggerfactory.cpp.o.d"
+  "CMakeFiles/logger_manager.dir/spdlogmessage.cpp.o"
+  "CMakeFiles/logger_manager.dir/spdlogmessage.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/logger_manager.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/test/CMakeFiles/test_spdlog.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for test_spdlog.
+# This may be replaced when dependencies are built.
